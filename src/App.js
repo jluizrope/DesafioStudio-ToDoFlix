d6a31b7f9b51e3f0738c8a0fa79def16cc1493f5
vai na web <vainawebcpx@gmail.com>
2020-01-27 12:57:22 -0300

    Trabalhei o modal, conforme o que o Prof Cícero explicava na aula, e corrigi erros, inclui com a ajuda do Prof assistente Cássio, hoje, dia 27 de janeiro de 2020 - segunda-feira.

--- src/App.js
+++ src/App.js
@@ -9,26 +9,71 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      modalOpen: false,
+      list: [],
+      modal: false,
+
     }
+
+   }
+
+
+  handleModal = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
   }
 
-  handleModal = () => {
-    this.setState({
-      modalOpen: !this.state.modalOpen,
-    });
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const {list} = this.state;
+
+    // localStorage
+
+    list.push({
+      name: this.inputText.value,
+      img: this.inputImg.file 
+    })
+    
+    
   }
+
+
+  renderModal = () => (
+    <div className='overlay' onClick={this.handleModal}>
+
+      <form onSubmit={this.handleSubmit}>
+        <input ref={node => this.inputText = node} required autoFocus/>
+        <input type='file' accept={'jpg', 'jpeg', 'svg'} ref={node => this.inputText = node}></input>
+        <button>Add</button>
+
+      </form>
+    </div>
+  )    
+  
 
   render() {
     return (
       <section>
-        <div>                
-                      
+        <div>
+          <button onClick={this.handleModal}>Clickhere</button> 
+          {this.state.modal ? this.renderModal() : null}
+          {this.state.list.map((item, index) => {
+            <div key={index}>
+              <img src='film.img'/>
+              <p>{item}</p>
+            </div>
+          })}
         </div>
       </section>
-
     )
   }
 }
                                                 
 export default App;
+ 
+       
